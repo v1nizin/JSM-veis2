d5e6171a839b7b7b8eb6b372ad8d3f53d1f8f88f
v1nizin <viniwick@outlook.com>
2023-09-06 16:38:53 -0300

    first commit

---
+++ vue-project/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Home from "./components/Home.vue"
+import Nav from "./components/Nav.vue";
+import Login from "./components/Login.vue"
+
+</script>
+
+<template>
+    <Nav></Nav>
+    <Login></Login>
+</template>
+
+<style>
+    #app {
+        padding: 0px;
+        margin: 0px;
+    }
+    
+
+</style>
